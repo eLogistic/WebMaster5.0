83a061814a74a58a61a3f3046c732612b4627f88
evasco23 <edwin.vasco@gmail.com>
2022-09-04 13:25:47 -0500

    creacion de repositorio e interfaz para conexion a base de datos de supabase

--- src/main/java/com/webmasters50/eLogistic/controladores/ControlEmpleados.java
+++ src/main/java/com/webmasters50/eLogistic/controladores/ControlEmpleados.java
@@ -4,15 +4,19 @@ package com.webmasters50.eLogistic.controladores;
 import com.webmasters50.eLogistic.entidades.Empleado;
 import com.webmasters50.eLogistic.servicios.ServiciosEmpleados;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 public class ControlEmpleados {
-    //pruebas de visualizacion
 
 /*
+    //pruebas de visualizacion en local
+
+
     @GetMapping("/vista1")
     public String visualizar(){
         return "Primera vista de web Spring Boot";
@@ -23,9 +27,6 @@ public class ControlEmpleados {
         Empleado e1 = new Empleado("Edwin" , "evasco%gmail.com", "coop", "cajero",null);
         return e1;
     }
-
- */
-
     ServiciosEmpleados services;
 
     //constructor de controlador
@@ -38,6 +39,29 @@ public class ControlEmpleados {
      return  this.services.getListaEmpleados();
     }
 
+ */
+
+    //nuevo codigo implementando herramientas de jpa
+
+    ServiciosEmpleados servicesE;
+
+    //constructor
+
+    public ControlEmpleados(ServiciosEmpleados servicesE){
+        this.servicesE=servicesE;
+
+    }
+
+    @GetMapping("/empleados")
+
+    public List<Empleado> listaEmpleados(){
+        return this.servicesE.getListaEmpleados();
+    }
+
+    @PostMapping("/empleados")
+    public Empleado crearEmpleado(@RequestBody Empleado e){
+        return this.servicesE.crearEmpleado(e);
+    }
 
 
 }
--- src/main/java/com/webmasters50/eLogistic/servicios/ServiciosEmpleados.java
+++ src/main/java/com/webmasters50/eLogistic/servicios/ServiciosEmpleados.java
@@ -2,34 +2,65 @@ package com.webmasters50.eLogistic.servicios;
 
 import com.webmasters50.eLogistic.entidades.Empleado;
 import com.webmasters50.eLogistic.entidades.Empresa;
+import com.webmasters50.eLogistic.repositorios.RepositorioEmpleados;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class ServiciosEmpleados {
 
-    Empleado empleado1;
-    Empleado empleado2;
-    List<Empleado> listaEmpleados;
+    /*
+    // el siguiente codigo se utilizo para hacer pruebas de insercion de objetos en local, se deja comentado para
+    // posibles consultas de funcionamiento del codigo
+    //
+    // Empleado empleado1;
+    // Empleado empleado2;
+    // List<Empleado> listaEmpleados;
+    //
+    // //se crea atributo para crear la relacion con ServiciosEmpresas
+    // ServiciosEmpresas empresaEmpleado = new ServiciosEmpresas();
+    //
+    // constructor de objetos en local
+    //
+    //     public ServiciosEmpleados() {
+    //     Empresa empresa1 = this.empresaEmpleado.e1;
+    //     this.empleado1 = new Empleado("Edwin" , "evasco%gmail.com", "coop", "cajero",empresa1);
+    //     this.empleado2 = new Empleado("Marcela>" , "marcela%gmail.com", "coop", "cajero",null);
+    //     this.listaEmpleados = new ArrayList<>();
+    //     listaEmpleados.add(empleado1);
+    //     listaEmpleados.add(empleado2);
+    // }
+    //
 
-    //se crea atributo para crear la relacion con ServiciosEmpresas
-    ServiciosEmpresas empresaEmpleado = new ServiciosEmpresas();
+     */
 
 
-    //constructor
-    public ServiciosEmpleados() {
-        Empresa empresa1 = this.empresaEmpleado.e1;
-        this.empleado1 = new Empleado("Edwin" , "evasco%gmail.com", "coop", "cajero",empresa1);
-        this.empleado2 = new Empleado("Marcela>" , "marcela%gmail.com", "coop", "cajero",null);
-        this.listaEmpleados = new ArrayList<>();
-        listaEmpleados.add(empleado1);
-        listaEmpleados.add(empleado2);
+    // atributos utilizando el repositorio
+
+    private RepositorioEmpleados repositorioEm;
+
+    //Consutructor usando repositorio
+
+    public ServiciosEmpleados(RepositorioEmpleados repositorioEm){
+        this.repositorioEm = repositorioEm;
     }
 
     //metodo getter para obtener lista empleados
 
+    //metodo para visualizar a partir del metodo get
+
     public List<Empleado> getListaEmpleados() {
-    return this.listaEmpleados;
+    return this.repositorioEm.findAll();
     }
+
+    //metodo para crear a partir del post
+
+    public Empleado crearEmpleado(Empleado nuevoEmpleado){
+        return this.repositorioEm.save(nuevoEmpleado);
+    }
+
+
 }
 
